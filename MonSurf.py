3b95bcfc8262c8b2786c1fdf29265f253a7ef4de
CrazyCoder009 <nairrohit3@gmail.com>
2020-05-01 21:29:57 +0530

    First version release

---
+++ MonSurf.py
@@ -0,0 +1,11 @@
+import shodan
+print("MonSurf is a Web surfing tool that searches for open/vulnerable IP Addresses, ports, webcams, security cameras, satellites and IoT Devices connected over the Internet. Keep in mind that not all features work properly and requires special care, if you emcounter any issues please do share them at the issues sections. Happy Hunting!;) ")
+print(" \n")
+Shodan_API="syeCnFndQ8TE4qAGvhm9nZLBZOBgoLKd"
+api = shodan.Shodan(Shodan_API)
+
+results=api.search(input("Search for: "))
+
+for result in results['matches']:
+        print('IP %s' % result['ip_str'])
+        print(result['data'])
